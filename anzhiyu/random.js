8afb01059926f06296b4475f0023b7f980caeda7
JIUMU07 <huangtinghui07@outlook.com>
2024-12-12 19:11:06 +0800

    Site updated: 2024-12-12 19:11:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/11/My-friend/","2024/12/11/hello-world/"];function toRandomPost(){
+var posts=["2024/12/11/hello-world/","2024/12/11/My-friend/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
